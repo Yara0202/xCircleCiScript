04e6e4c3e727835ce88ee8af2187438d4f112789
Wrdn28 <ifloraxd@gmail.com>
2023-02-12 23:18:59 +0700

    change some source

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,13 +4,13 @@ git clone --depth=1 https://github.com/Wrdn28/Kernel_Asus_X01AD-X.git X01AD
 git clone --depth=1 https://github.com/arifmndr17/hyper-clang clang
 
 # Main
-KERNEL_NAME=XnovaTest # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=Extrajoss-OC-Ricedroid # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
 CLANG_ROOTDIR=$(pwd)/clang # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER=hirokixd # Change with your own name or else.
-export KBUILD_BUILD_HOST=hkprjct # Change with your own hostname.
+export KBUILD_BUILD_USER=Arif # Change with your own name or else.
+export KBUILD_BUILD_HOST=DroidDev # Change with your own hostname.
 IMAGE=$(pwd)/X01AD/out/arch/arm64/boot/Image.gz-dtb
 DATE=$(date +"%F-%S")
 START=$(date +"%s")
@@ -53,7 +53,7 @@ function compile() {
 	finerr
 	exit 1
    fi
-    git clone --depth=1 https://github.com/Wrdn28/AnyKernel3.git AnyKernel
+    git clone --depth=1 https://github.com/ArifDroidDev/AnyKernel3.git AnyKernel
 	cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 
@@ -65,7 +65,7 @@ function push() {
         -F chat_id="$chat_id" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7 (lavender)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Asus Zenfone Max M2 (X01AD)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 
 }
 # Fin Error
