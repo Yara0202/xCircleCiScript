3b8d43787692ae742817608c63ceab9638e0b750
Wrdn28 <ifloraxd@gmail.com>
2023-02-14 00:04:44 +0700

    change clang compiler

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
 git clone --depth=1 https://github.com/Wrdn28/kernel_asus_X01AD.git X01AD
-git clone --depth=1 https://gitlab.com/Amritorock/clang-r450784d.git clang
+git clone --depth=1 https://github.com/Wrdn28/clang-r416183b1.git clang
 
 # Main
 KERNEL_NAME=Best-x-v2 # IMPORTANT ! Declare your kernel name
