a57cdce0ae502669230e41034ca28cde0d194d6b
Leonard <86230878+Wrdn28@users.noreply.github.com>
2023-02-21 02:01:52 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
-git clone --depth=1 https://github.com/Wrdn28/android_kernel_asus_X01AD.git X01AD
+git clone --depth=1 https://github.com/Wrdn28/kernel_asus_X01AD X01AD
 git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang clang
 
 # Main
