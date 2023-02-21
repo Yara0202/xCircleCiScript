f583cb870ecee652cfe1efb20bae299dc0c8541f
Leonard <86230878+Wrdn28@users.noreply.github.com>
2023-02-21 15:18:01 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -4,7 +4,7 @@ git clone --depth=1 https://github.com/Wrdn28/kernel_asus_X01AD X01AD
 git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang clang
 
 # Main
-KERNEL_NAME=OrionX_BetaTest # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=OrionX_Beta # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
